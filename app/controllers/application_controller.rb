4d6c6f950d0b2b95cd4ee6c55b738f01c1b30a12
USER <81149239+ryusen1000@users.noreply.github.com>
2021-04-21 10:26:19 +0900

    ユーザー登録ビューファイルの設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       p(p(username) == p(ENV['FURIMA_NAME'])) && p(p(password) == p(ENV['FURIMA_PASS']))
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:])
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:])
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:])
   end
 end
