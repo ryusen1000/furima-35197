db178f5e95b150716e5f0c2e771da6e1ecefddcc
USER <81149239+ryusen1000@users.noreply.github.com>
2021-04-21 17:31:36 +0900

    入力データのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, :password_confirmation, format:{with:/\A([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])\z/i}
+  validates :first_name, presence: true, format:{with:/\A[ぁ-んァ-ヴ一-龥]+\z/}
+  validates :family_name, presence: true, format:{with:/\A[ぁ-んァ-ヴ一-龥]+\z/}
+  validates :first_name_kana, presence: true, format:{with:/\A[ァ-ヴ]+\z/}
+  validates :family_name_kana, presence: true, format:{with:/\A[ァ-ヴ]+\z/}
+  validates :birth, presence: true
+  
 end
