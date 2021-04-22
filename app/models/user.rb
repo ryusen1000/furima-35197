707c5441c1500e4c989d48ffe9d9e115550117d1
USER <81149239+ryusen1000@users.noreply.github.com>
2021-04-22 12:46:46 +0900

    USERモデルのリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :password, :password_confirmation, format:{with:/\A([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])\z/i}
-  validates :first_name, presence: true, format:{with:/\A[ぁ-んァ-ヴ一-龥]+\z/}
-  validates :family_name, presence: true, format:{with:/\A[ぁ-んァ-ヴ一-龥]+\z/}
-  validates :first_name_kana, presence: true, format:{with:/\A[ァ-ヴ]+\z/}
-  validates :family_name_kana, presence: true, format:{with:/\A[ァ-ヴ]+\z/}
-  validates :birth, presence: true
-  
+  with_options presence: true do
+    validates :nickname, :birth
+
+    with_options format:{with:/\A([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])\z/i} do
+      validates :password, :password_confirmation
+    end
+
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      validates :first_name, :family_name
+    end
+
+    with_options format:{with:/\A[ァ-ヶ]+\z/} do
+    validates :first_name_kana, :family_name_kana
+    end
+  end
 end
