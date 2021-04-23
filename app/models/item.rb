c67b8d7daf530d54d9e44c993c983dfe2f4a5d24
USER <81149239+ryusen1000@users.noreply.github.com>
2021-04-23 17:46:03 +0900

    出品機能のビューファイルの反映

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,14 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :item_name, :text, :price, :image
+    validates :item_name, :text, :image
 
     with_options numericality: { other_then: 1 } do
       validates :category_id, :item_status_id, :ship_cost_id, :prefecture_id, :days_to_ship_id
     end
 
+    with_options numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } do
+      validates :price
+    end
   end
 end
