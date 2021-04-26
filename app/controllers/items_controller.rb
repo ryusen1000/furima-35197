f1f80e75ddea3fee57cfc4cbaa1c0989db0f7473
USER <81149239+ryusen1000@users.noreply.github.com>
2021-04-26 18:13:01 +0900

    コントローラーのdestroyアクションと詳細ページのリンク作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -31,6 +31,14 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.delete
+      redirect_to root_path
+    else
+      render item_path
     end
   end
 
